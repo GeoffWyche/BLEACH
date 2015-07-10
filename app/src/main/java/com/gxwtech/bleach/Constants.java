390007f6fe1fd58845e881a710adefc1ccd9279e
Geoff Wyche <geoff@gxwtech.com>
2015-07-10 15:04:59 -0500

    made preferenceBackedStorage generic

---
+++ app/src/main/java/com/gxwtech/bleach/Constants.java
@@ -0,0 +1,8 @@
+package com.gxwtech.bleach;
+
+/**
+ * Created by geoff on 7/10/15.
+ */
+public abstract class Constants {
+    public static final String PREF_STORAGE_NAME = "RileyLinkPrefStorage";
+}
